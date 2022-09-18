1e706c969a52adaee58e3bc633cdb7740b096e94
MallikarjungoudA <aamallikarjuna18@gmail.com>
2022-09-18 20:58:46 +0530

    added the migration

---
+++ EFCoreWebAppDemo/EFcoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EFCoreWebAppDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebAppDemo
+{
+    public class EFcoreDbContext : DbContext
+    {
+        public EFcoreDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> student {get; set;}
+    }
+}
---
+++ EFCoreWebAppDemo/Entities/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreWebAppDemo.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int age { get; set; }
+        public string name { get; set; } 
+    }
+}
